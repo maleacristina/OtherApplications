825d4b161c697644b220fe392764819b7fa0d177
Cristina <maleacristinaecaterina@gmail.com>
2017-12-02 22:05:57 +0200

    Breeds of dogs

--- Rasele_de_caini/main.cpp
+++ Rasele_de_caini/main.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-#include <string.h>
-#include <conio.h>
-#include "Caine.h"
-#include "Cainecupete.h"
-#include "Cainefarapete.h"
-/*
-3. Să se construiască o ierarhie de clase care să descrie rasele de câini. Se consideră clasa
+/*3. Să se construiască o ierarhie de clase care să descrie rasele de câini. Se consideră clasa
 de bază Caine, iar din ea se derivează clasa CaineCuPete şi clasa CaineFaraPete. Despre
 un câine se cunosc următoarele informaţii: numele, înălţime, greutate, vârstă, culoare.
 Despre câinii cu pete se ştie şi numărul de pete. Să se defineasca pentru fiecare clasa o
 functie de citire a datelor, respectiv una de afisare; ambele functii vor fi functii virtuale.
 afisare.
 */
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+#include "Caine.h"
+#include "Cainecupete.h"
+#include "Cainefarapete.h"
+
 using namespace std;
 int main()
 {
